05cc2f2c871accd042a30c03e960581d0199d693
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-12-01 21:35:46 -0300

    Acompanhamento do final do capitulo 4.

--- CDCFoods/CDCFoods/Pages/Entregadores/EntregadoresNewPage.xaml.cs
+++ CDCFoods/CDCFoods/Pages/Entregadores/EntregadoresNewPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CDCFoods.Dal;
+using CDCFoods.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +14,33 @@ namespace CDCFoods.Pages.Entregadores
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EntregadoresNewPage : ContentPage
     {
+        private EntregadorDal dalEntregadores = EntregadorDal.GetInstance();
         public EntregadoresNewPage()
         {
             InitializeComponent();
+            PreparaParaNovoEntregador();
         }
+
 
         private async void BtnGravarClick(object sender, EventArgs e)
         {
-            await DisplayAlert("Aa", "aa", "Ok");
+            if (string.IsNullOrEmpty(nome.Text.Trim()) || string.IsNullOrEmpty(telefone.Text.Trim()))
+            {
+                await DisplayAlert("Erro", "Você precisa informar o nome e o telefone do entregador", "Ok");
+            }
+            else
+            {
+                dalEntregadores.AddEntregador(new Entregador() { Id = Convert.ToInt32(identregador.Text), Nome = nome.Text, Telefone = telefone.Text });
+                PreparaParaNovoEntregador();
+            }
+        }
+
+        private void PreparaParaNovoEntregador()
+        {
+            var novoId = dalEntregadores.GetAll().Max(x => x.Id) + 1;
+            identregador.Text = novoId.ToString().Trim();
+            nome.Text = string.Empty;
+            telefone.Text = string.Empty;
         }
     }
 }
